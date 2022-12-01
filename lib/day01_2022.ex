8de6a387f4c660c91a347dd3e2a868969e151829
Sarah Doire <sarah.doire@postlight.com>
2022-12-01 10:37:02 -0600

    day 1 2022

---
+++ lib/day01_2022.ex
@@ -0,0 +1,53 @@
+defmodule Day01_2022 do
+  def findMaxCalorie() do
+    contents = File.read!("lib/input/y2022_d01.txt")
+
+    split =
+      String.split(contents, "\n")
+      |> Enum.chunk_by(&(&1 == ""))
+      |> Enum.reject(&(&1 == [""]))
+      |> Enum.reduce(0, fn elf, acc ->
+        calories =
+          elf
+          |> Enum.map(&String.to_integer/1)
+          |> Enum.sum
+
+        if calories > acc do
+          calories
+        else
+          acc
+        end
+      end)
+
+    IO.inspect(split)
+  end
+
+  def findTop3MaxCalorie() do
+    contents = File.read!("lib/input/y2022_d01.txt")
+
+    split =
+      String.split(contents, "\n")
+      |> Enum.chunk_by(&(&1 == ""))
+      |> Enum.reject(&(&1 == [""]))
+      |> Enum.reduce([0, 0, 0], fn elf, acc ->
+        calories =
+          elf
+          |> Enum.map(&String.to_integer/1)
+          |> Enum.sum
+
+        cond do
+          calories > Enum.at(acc, 0) ->
+            [calories, Enum.at(acc, 0), Enum.at(acc, 1)]
+          calories > Enum.at(acc, 1) ->
+            [Enum.at(acc, 0), calories, Enum.at(acc, 1)]
+          calories > Enum.at(acc, 2) ->
+            [Enum.at(acc, 0), Enum.at(acc, 1), calories]
+          true ->
+            acc
+          end
+
+      end)
+
+    IO.inspect(Enum.sum(split))
+  end
+end
---
+++ lib/day_03.ex
@@ -0,0 +1,18 @@
+defmodule Day3 do
+  def calc_position(input) do
+    contents = File.read!(input)
+
+    array =
+      String.split(contents, "\n")
+
+    input_length = String.length(List.first(array))
+    # repeater_length = input_length - 1
+    # accumulator = List.duplicate(0, input_length)
+    result =
+      array
+      |> Enum.map(&String.graphemes/1)
+      |> Enum.zip()
+
+    result
+  end
+end
